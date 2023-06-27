568610b8e72720a84e75583e5a6dffdf4fc750ec
Velasques <andrevelasques7@gmail.com>
2023-06-27 16:05:04 -0300

    Mudança de bibliotecas

--- init.py
+++ init.py
@@ -1,73 +1,41 @@
-import pygame
-import sys
+import tkinter as tk
 import random
 
-# Inicialização do Pygame
-pygame.init()
+# Criação da janela
+window = tk.Tk()
+window.title("Meu Jogo")
+window.geometry("800x600")
+# Define a cor de fundo da janela
+window.configure(background="#3a9c95")  # Substitua pela cor desejada
 
-# Definição das dimensões da janela
-width = 800
-height = 600
 
 # Cores
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+BLACK = "#000000"
+WHITE = "#FFFFFF"
 
-# Criação da janela
-window = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Meu Jogo")
-
-# Carregamento e redimensionamento da imagem
-image = pygame.image.load("imgs/marvin_2.png")
-image = pygame.transform.scale(image, (100, 100))
-
-# Posição inicial da imagem
-image_x = width // 2 - 50
-image_y = height // 2 - 50
-
+# Criação de um rótulo para exibir a pontuação
 pontuacao = 0
+pontuacao_label = tk.Label(window, text="Pontuação: " + str(pontuacao))
+pontuacao_label.pack()
 
+# Carregamento e exibição da imagem
+image = tk.PhotoImage(file="imgs/marvin_2.png")
+image_label = tk.Label(window, image=image)
+image_label.place(x=350, y=250)  # Define a posição inicial da imagem
 
-def atualizar_pontuacao():
-    fonte = pygame.font.Font(None, 50)  # Escolha a fonte e o tamanho desejados
-    texto_pontuacao = fonte.render("Pontuação: " + str(pontuacao), True, WHITE)
-    window.blit(texto_pontuacao, (10, 10))  # Define a posição do texto na tela
+def mover_imagem(event):
+    global pontuacao
 
-def mover_imagem():
-    # Define as novas coordenadas X e Y para a imagem
-    new_x = random.randint(0, width - 100)
-    new_y = random.randint(0, height - 100)
+    # Gera novas coordenadas X e Y para a imagem
+    new_x = random.randint(0, 700)
+    new_y = random.randint(0, 500)
+    image_label.place(x=new_x, y=new_y)
 
-    # Atualiza as coordenadas da imagem
-    global image_x, image_y
-    image_x = new_x
-    image_y = new_y
+    # Incrementa a pontuação ao mover a imagem
+    pontuacao += 1
+    pontuacao_label.config(text="Pontuação: " + str(pontuacao))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-           mouse_x, mouse_y = pygame.mouse.get_pos()
-           if image_x <= mouse_x <= image_x + 100 and image_y <= mouse_y <= image_y + 100:
-              # Aqui você pode adicionar o comportamento desejado ao clicar na imagem
-              pontuacao += 1  # Incrementa a pontuação ao clicar na imagem
-              mover_imagem()  # Move a imagem para uma nova posição
+# Associa a função mover_imagem ao evento de clique na imagem
+image_label.bind("<Button-1>", mover_imagem)
 
-
-
-    # Preenchimento do fundo com a cor preta
-    window.fill(BLACK)
-
-    # Desenho da imagem
-    window.blit(image, (image_x, image_y))
-
-    # Atualização da pontuação
-    atualizar_pontuacao()
-
-    # Atualização da janela
-    pygame.display.update()
-
-
-
+window.mainloop()
