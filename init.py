6626f9014ce5ddfe914cec50fc61800ef60b16dc
Velasques <andrevelasques7@gmail.com>
2023-06-27 17:37:14 -0300

    Mudança de bibliotecas

--- init.py
+++ init.py
@@ -1,12 +1,29 @@
 import tkinter as tk
 import random
+from PIL import Image
+
+image_jpeg = Image.open("imgs/background.jpg")
+
+# Define o caminho e o nome do arquivo PNG de saída
+output_path = "imgs/background.png"
+
+# Salva a imagem JPEG como PNG
+image_jpeg.save(output_path, "PNG")
 
 # Criação da janela
 window = tk.Tk()
 window.title("Meu Jogo")
-window.geometry("800x600")
+window.geometry("1300x750")
 # Define a cor de fundo da janela
 window.configure(background="#3a9c95")  # Substitua pela cor desejada
+
+# Criação do Canvas
+canvas = tk.Canvas(window, width=1200, height=675)
+canvas.pack()
+
+# Carregamento e exibição da imagem de fundo
+background_image = tk.PhotoImage(file="imgs/background.png")
+canvas.create_image(0, 0, anchor="nw", image=background_image)
 
 
 # Cores
@@ -19,7 +36,7 @@ pontuacao_label = tk.Label(window, text="Pontuação: " + str(pontuacao))
 pontuacao_label.pack()
 
 # Carregamento e exibição da imagem
-image = tk.PhotoImage(file="imgs/marvin_2.png")
+image = tk.PhotoImage(file="imgs/Target-icon.png")
 image_label = tk.Label(window, image=image)
 image_label.place(x=350, y=250)  # Define a posição inicial da imagem
 
